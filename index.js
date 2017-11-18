9d5e71da06bc59589827b333305b3c44c2bae1c5
canddduo1403 <nantapornd56@email.nu.ac.th>
2017-11-18 15:36:37 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+
+const async = require('async')
+const request = require('request')
+const cheerio = require('cheerio')
+
+const fs = require('fs');
+
+const items = [
+    {
+        name: "zuka",
+        url: "http://rov.wikia.com/wiki/Zuka"
+    },
+    {
+        name: "murad",
+        url: "http://rov.wikia.com/wiki/Murad"
+    },
+    {
+        name: "preyta",
+        url: "http://rov.wikia.com/wiki/Praeyta"
+    }
+
+]
+
+
+
+const q = async.queue((task, callback) => {
+
+    request(task.url, (err, res, body) => {
+        $ = cheerio.load(body)
+        //console.log($('#mw-content-text').text())
+
+        const text = $('#mw-content-text p').text();
+        fs.writeFile(task.name + ".txt", text, (err) => {
+            if (err) {
+                console.log(err)
+                callback()
+            }
+            console.log("Save file complete")
+            callback()
+        })
+
+    })
+
+    // request(url, (err, res, body) => {
+
+    //     fs.writeFile("web" + i + ".html", body, (err) => {
+    //         if (err) {
+    //             console.log(err)
+    //             callback()
+    //         }
+    //         console.log("Save file complete")
+    //         callback()
+    //     })
+    // })
+})
+
+q.drain = () => {
+    console.log('all items have been process');
+}
+
+q.push(items, (err) => {
+    console.log('finised process itens')
+})
